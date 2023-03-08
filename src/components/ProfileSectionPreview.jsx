efd306238b017b6bb2a4fac17263f40b1adefc42
nronzel <86695181+nronzel@users.noreply.github.com>
2023-03-08 15:36:29 -0500

    add: resume preview components

---
+++ src/components/ProfileSectionPreview.jsx
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import { Box, Flex, Heading, VStack, Text, Spacer } from "@chakra-ui/react";
+
+export class ProfileSectionPreview extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <Flex
+        justifyContent="space-between"
+        w="100%"
+        alignItems="center"
+        pb={2}
+        borderBottom="4px solid"
+        borderColor="blue.700"
+      >
+        <Box>
+          <Heading size="xl" fontWeight="thin">
+            {this.props.name.split(" ")[0]}
+          </Heading>
+          <Heading size="2xl" color="blue.700">
+            {this.props.name.split(" ")[1]}
+          </Heading>
+          <Text fontSize="xl" as="i" fontWeight="thin">
+            {this.props.title}
+          </Text>
+        </Box>
+        <Spacer />
+        <VStack w="50%">
+          <Text w="100%" textAlign={"right"} color="gray.600" fontWeight="thin">
+            {this.props.email}
+          </Text>
+          <Text w="100%" textAlign={"right"} color="gray.600" fontWeight="thin">
+            {this.props.phone}
+          </Text>
+          <Text w="100%" textAlign={"right"} color="gray.600" fontWeight="thin">
+            {this.props.link}
+          </Text>
+        </VStack>
+      </Flex>
+    );
+  }
+}
+
+export default ProfileSectionPreview;
---
+++ src/components/ResumePreview.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import ProfileSectionPreview from "./ProfileSectionPreview";
+import { Flex } from "@chakra-ui/react";
+
+export class ResumePreview extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <Flex
+        direction="column"
+        border="2px solid"
+        borderColor="gray.700"
+        p={8}
+        borderRadius={5}
+        w="595px"
+        bgColor="white"
+        color="black"
+        alignItems="center"
+      >
+        <ProfileSectionPreview
+          name={this.props.name}
+          email={this.props.email}
+          phone={this.props.phone}
+          link={this.props.link}
+          title={this.props.title}
+          description={this.props.description}
+        />
+      </Flex>
+    );
+  }
+}
+
+export default ResumePreview;
