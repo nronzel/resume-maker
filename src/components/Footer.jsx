aed258a4ec9662399a19d9b9f9b3affa96a404f4
nronzel <86695181+nronzel@users.noreply.github.com>
2023-03-08 15:40:41 -0500

    refactor: footer position using aboslute position

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,13 @@
-import { Flex, Text, useColorModeValue } from "@chakra-ui/react";
+import { Center, Text, useColorModeValue } from "@chakra-ui/react";
 import React from "react";
 
 function Footer() {
   return (
-    <Flex justifyContent="center" alignItems="center" h="50px">
+    <Center h="50px" w="100%" position="absolute" bottom="0">
       <Text color={useColorModeValue("gray.400", "gray.600")}>
         nronzel &copy; 2023 All Rights Reserved{" "}
       </Text>
-    </Flex>
+    </Center>
   );
 }
 
