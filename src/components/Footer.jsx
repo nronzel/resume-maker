545e9d756c47487b2eaa7bac0885dece2d6933bd
nronzel <86695181+nronzel@users.noreply.github.com>
2023-03-08 10:50:48 -0500

    add: footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import { Flex, Text, useColorModeValue } from "@chakra-ui/react";
+import React from "react";
+
+function Footer() {
+  return (
+    <Flex justifyContent="center" alignItems="center" h="50px">
+      <Text color={useColorModeValue("gray.400", "gray.600")}>
+        nronzel &copy; 2023 All Rights Reserved{" "}
+      </Text>
+    </Flex>
+  );
+}
+
+export default Footer;
