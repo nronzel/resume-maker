b0d4b866acf7370e2dea3935d94a5af51867cac3
nronzel <86695181+nronzel@users.noreply.github.com>
2023-03-08 10:53:01 -0500

    add: import and render newest components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,16 @@
 import React, { Component } from "react";
 import { Flex } from "@chakra-ui/react";
 import Header from "./Header";
+import MainSection from "./MainSection";
+import Footer from "./Footer";
 
 export default class App extends Component {
   render() {
     return (
-      <Flex h="100vh" direction="column">
+      <Flex h="100vh" direction="column" gap={5}>
         <Header />
-        {/* <ResumeForm /> */}
-        {/* <ResumePreview /> */}
+        <MainSection />
+        <Footer />
       </Flex>
     );
   }
