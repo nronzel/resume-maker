f622c54c6221a532a35ceb463b621841cfb5c307
nronzel <86695181+nronzel@users.noreply.github.com>
2023-03-08 10:54:00 -0500

    refactor: added spacer

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,13 @@
 import React, { Component } from "react";
-import { Flex, Heading } from "@chakra-ui/react";
+import { Flex, Heading, Spacer } from "@chakra-ui/react";
 import ThemeSwitcher from "./ThemeSwitcher";
 
 export class Header extends Component {
   render() {
     return (
       <Flex bg="cyan.800" p="6px 12px" alignItems="center">
-        <Heading fontWeight={800} flexGrow="1">
-          ResumeBuildr
-        </Heading>
+        <Heading fontWeight="extrabold">ResumeBuildr</Heading>
+        <Spacer />
         <ThemeSwitcher />
       </Flex>
     );
