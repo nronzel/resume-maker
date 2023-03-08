5e97ebdf34c8ebe5737848a1acb3d0314999c54f
nronzel <86695181+nronzel@users.noreply.github.com>
2023-03-08 15:42:31 -0500

    add: component that holds main content of page

--- src/components/MainSection.jsx
+++ src/components/MainSection.jsx
@@ -1,15 +1,43 @@
 import { Flex } from "@chakra-ui/react";
-import React from "react";
+import React, { Component } from "react";
 import ResumeForm from "./ResumeForm";
-// import ResumePreview from "./ResumePreview";
+import ResumePreview from "./ResumePreview";
 
-function MainSection() {
-  return (
-    <Flex justifyContent="space-around" flexGrow="1">
-      <ResumeForm />
-      {/* <ResumePreview /> */}
-    </Flex>
-  );
+export class MainSection extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "Art Vandelay",
+      email: "avandalay@vandelayindustries.com",
+      phone: "555-555-5555",
+      link: "https://vandelayindustries.com",
+      title: "Import/Exporter",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur, ipsa!",
+    };
+  }
+
+  handleInputChange = (e) => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  };
+
+  render() {
+    const { name, email, phone, link, title, description } = this.state;
+    return (
+      <Flex justifyContent="center" gap={5}>
+        <ResumeForm handleChange={this.handleInputChange} />
+        <ResumePreview
+          name={name}
+          email={email}
+          phone={phone}
+          link={link}
+          title={title}
+          description={description}
+        />
+      </Flex>
+    );
+  }
 }
 
 export default MainSection;
