ab2ab79d1181535abf3fa2df467e993014833214
nronzel <86695181+nronzel@users.noreply.github.com>
2023-03-11 17:40:55 -0500

    updated header width

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,10 +5,17 @@ import ThemeSwitcher from "./ThemeSwitcher.jsx";
 export class Header extends Component {
   render() {
     return (
-      <Flex bg="purple.600" p="6px 12px" alignItems="center">
-        <Heading fontWeight="extrabold">ResumeBuildr</Heading>
-        <Spacer />
-        <ThemeSwitcher />
+      <Flex
+        bg="purple.600"
+        p="6px 12px"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <Flex maxW="1200px" w="90vw">
+          <Heading fontWeight="extrabold">ResumeBuildr</Heading>
+          <Spacer />
+          <ThemeSwitcher />
+        </Flex>
       </Flex>
     );
   }
