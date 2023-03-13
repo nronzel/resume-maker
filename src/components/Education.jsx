d61175fa914b4c838d753b8e25781757ccd517f5
nronzel <86695181+nronzel@users.noreply.github.com>
2023-03-13 16:53:49 -0400

    refactored to functional component

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,58 +1,30 @@
-import React, { Component } from "react";
 import { Input, Flex } from "@chakra-ui/react";
+import React from "react";
 
-class Education extends Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    const { id, handleChange } = this.props;
-    return (
-      <Flex direction="column" gap={4} mb={10}>
+function Education({ id, handleChange }) {
+  return (
+    <Flex direction="column" gap={4} mb={10}>
+      <Input
+        defaultValue=""
+        borderBottom="1px solid"
+        p="3px"
+        variant="flushed"
+        colorScheme="purple"
+        name="school"
+        focusBorderColor="purple.400"
+        placeholder="institution name"
+        onChange={(e) => handleChange(e, id)}
+      ></Input>
+      <Flex gap={5}>
         <Input
           defaultValue=""
           borderBottom="1px solid"
           p="3px"
           variant="flushed"
           colorScheme="purple"
-          name="school"
+          name="start"
           focusBorderColor="purple.400"
-          placeholder="institution name"
-          onChange={(e) => handleChange(e, id)}
-        ></Input>
-        <Flex gap={5}>
-          <Input
-            defaultValue=""
-            borderBottom="1px solid"
-            p="3px"
-            variant="flushed"
-            colorScheme="purple"
-            name="start"
-            focusBorderColor="purple.400"
-            placeholder="start"
-            onChange={(e) => handleChange(e, id)}
-          ></Input>
-          <Input
-            defaultValue=""
-            borderBottom="1px solid"
-            p="3px"
-            variant="flushed"
-            colorScheme="purple"
-            name="end"
-            focusBorderColor="purple.400"
-            placeholder="end"
-            onChange={(e) => handleChange(e, id)}
-          ></Input>
-        </Flex>
-        <Input
-          defaultValue=""
-          borderBottom="1px solid"
-          p="3px"
-          variant="flushed"
-          colorScheme="purple"
-          name="fieldOfStudy"
-          focusBorderColor="purple.400"
-          placeholder="field of study"
+          placeholder="start"
           onChange={(e) => handleChange(e, id)}
         ></Input>
         <Input
@@ -61,14 +33,36 @@ class Education extends Component {
           p="3px"
           variant="flushed"
           colorScheme="purple"
-          name="degree"
+          name="end"
           focusBorderColor="purple.400"
-          placeholder="degree"
+          placeholder="end"
           onChange={(e) => handleChange(e, id)}
         ></Input>
       </Flex>
-    );
-  }
+      <Input
+        defaultValue=""
+        borderBottom="1px solid"
+        p="3px"
+        variant="flushed"
+        colorScheme="purple"
+        name="fieldOfStudy"
+        focusBorderColor="purple.400"
+        placeholder="field of study"
+        onChange={(e) => handleChange(e, id)}
+      ></Input>
+      <Input
+        defaultValue=""
+        borderBottom="1px solid"
+        p="3px"
+        variant="flushed"
+        colorScheme="purple"
+        name="degree"
+        focusBorderColor="purple.400"
+        placeholder="degree"
+        onChange={(e) => handleChange(e, id)}
+      ></Input>
+    </Flex>
+  );
 }
 
 export default Education;
