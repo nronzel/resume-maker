c5231dad79c87d6018a2869a70f309e3edae55fb
nronzel <86695181+nronzel@users.noreply.github.com>
2023-03-13 17:39:07 -0400

    refactored to functional component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,22 @@
-import React, { Component } from "react";
+import React from "react";
 import { Flex, Heading, Spacer } from "@chakra-ui/react";
 import ThemeSwitcher from "./ThemeSwitcher.jsx";
 
-export class Header extends Component {
-  render() {
-    return (
-      <Flex
-        bg="purple.600"
-        p="6px 12px"
-        alignItems="center"
-        justifyContent="center"
-      >
-        <Flex maxW="1200px" w="90vw">
-          <Heading fontWeight="extrabold">ResumeBuildr</Heading>
-          <Spacer />
-          <ThemeSwitcher />
-        </Flex>
+function Header() {
+  return (
+    <Flex
+      bg="purple.600"
+      p="6px 12px"
+      alignItems="center"
+      justifyContent="center"
+    >
+      <Flex maxW="1200px" w="90vw">
+        <Heading fontWeight="extrabold">ResumeBuildr</Heading>
+        <Spacer />
+        <ThemeSwitcher />
       </Flex>
-    );
-  }
+    </Flex>
+  );
 }
 
 export default Header;
--- src/components/MainInfoSection.jsx
+++ src/components/MainInfoSection.jsx
@@ -1,96 +1,91 @@
 import { Input, Textarea, Flex, Box } from "@chakra-ui/react";
-import React, { Component } from "react";
+import React from "react";
 
-export class MainInfoSection extends Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    return (
-      <Box mt={3}>
-        <Flex
-          gap={3}
-          alignItems="center"
-          flexWrap="wrap"
-          justifyContent="center"
-          direction="column"
-          w="100%"
-        >
-          <Flex w="100%" gap={4}>
-            <Input
-              defaultValue=""
-              borderBottom="1px solid"
-              p="3px"
-              variant="flushed"
-              onChange={this.props.handleChange}
-              name="firstName"
-              focusBorderColor="purple.400"
-              placeholder="first name"
-            ></Input>
-            <Input
-              defaultValue=""
-              borderBottom="1px solid"
-              p="3px"
-              variant="flushed"
-              onChange={this.props.handleChange}
-              name="lastName"
-              focusBorderColor="purple.400"
-              placeholder="last name"
-            ></Input>
-          </Flex>
+function MainInfoSection({ handleChange }) {
+  return (
+    <Box mt={3}>
+      <Flex
+        gap={3}
+        alignItems="center"
+        flexWrap="wrap"
+        justifyContent="center"
+        direction="column"
+        w="100%"
+      >
+        <Flex w="100%" gap={4}>
           <Input
             defaultValue=""
             borderBottom="1px solid"
             p="3px"
             variant="flushed"
-            name="email"
-            onChange={this.props.handleChange}
+            onChange={handleChange}
+            name="firstName"
             focusBorderColor="purple.400"
-            placeholder="email@email.com"
+            placeholder="first name"
           ></Input>
           <Input
             defaultValue=""
             borderBottom="1px solid"
             p="3px"
             variant="flushed"
-            name="phone"
-            onChange={this.props.handleChange}
+            onChange={handleChange}
+            name="lastName"
             focusBorderColor="purple.400"
-            placeholder="555-555-5555"
+            placeholder="last name"
           ></Input>
-          <Input
-            defaultValue=""
-            borderBottom="1px solid"
-            p="3px"
-            variant="flushed"
-            name="link"
-            onChange={this.props.handleChange}
-            focusBorderColor="purple.400"
-            placeholder="https://github.com/"
-          ></Input>
-          <Input
-            defaultValue=""
-            borderBottom="1px solid"
-            p="3px"
-            variant="flushed"
-            name="title"
-            onChange={this.props.handleChange}
-            focusBorderColor="purple.400"
-            placeholder="title"
-          ></Input>
-          <Textarea
-            placeholder="summary"
-            defaultValue=""
-            name="description"
-            size="sm"
-            onChange={this.props.handleChange}
-            h={150}
-            focusBorderColor="purple.400"
-          ></Textarea>
         </Flex>
-      </Box>
-    );
-  }
+        <Input
+          defaultValue=""
+          borderBottom="1px solid"
+          p="3px"
+          variant="flushed"
+          name="email"
+          onChange={handleChange}
+          focusBorderColor="purple.400"
+          placeholder="email@email.com"
+        ></Input>
+        <Input
+          defaultValue=""
+          borderBottom="1px solid"
+          p="3px"
+          variant="flushed"
+          name="phone"
+          onChange={handleChange}
+          focusBorderColor="purple.400"
+          placeholder="555-555-5555"
+        ></Input>
+        <Input
+          defaultValue=""
+          borderBottom="1px solid"
+          p="3px"
+          variant="flushed"
+          name="link"
+          onChange={handleChange}
+          focusBorderColor="purple.400"
+          placeholder="https://github.com/"
+        ></Input>
+        <Input
+          defaultValue=""
+          borderBottom="1px solid"
+          p="3px"
+          variant="flushed"
+          name="title"
+          onChange={handleChange}
+          focusBorderColor="purple.400"
+          placeholder="title"
+        ></Input>
+        <Textarea
+          placeholder="summary"
+          defaultValue=""
+          name="description"
+          size="sm"
+          onChange={handleChange}
+          h={150}
+          focusBorderColor="purple.400"
+        ></Textarea>
+      </Flex>
+    </Box>
+  );
 }
 
 export default MainInfoSection;
