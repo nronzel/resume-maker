955d6f477432ec30c92c918f47fb144930bb4805
nronzel <86695181+nronzel@users.noreply.github.com>
2023-03-13 17:07:09 -0400

    refactored into functional components

--- src/components/EducationPreview.jsx
+++ src/components/EducationPreview.jsx
@@ -1,35 +1,28 @@
-import React, { Component } from "react";
+import React from "react";
 import { Flex, Spacer, Text, HStack } from "@chakra-ui/react";
 
-class EducationPreview extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    const { id, school, degree, start, end, fieldOfStudy } =
-      this.props.education;
-    return (
-      <Flex w="100%" key={id} id={id} direction="column" mb={3}>
-        <Flex w="100%" mt={2}>
-          <Flex direction="column">
-            <Text fontSize="lg" fontWeight="semibold" color="blue.700">
-              {fieldOfStudy}
-            </Text>
-            <Text>{school}</Text>
-          </Flex>
-          <Spacer />
-          <Flex alignItems="flex-start">
-            <HStack>
-              <Text color="gray.500">{degree}</Text>
-              <Text fontWeight="thin">|</Text>
-              <Text as="i" fontWeight="thin">{`${start} - ${end}`}</Text>
-            </HStack>
-          </Flex>
+const EducationPreview = ({ education }) => {
+  const { id, school, degree, start, end, fieldOfStudy } = education;
+  return (
+    <Flex w="100%" key={id} id={id} direction="column" mb={3}>
+      <Flex w="100%" mt={2}>
+        <Flex direction="column">
+          <Text fontSize="lg" fontWeight="semibold" color="blue.700">
+            {fieldOfStudy}
+          </Text>
+          <Text>{school}</Text>
+        </Flex>
+        <Spacer />
+        <Flex alignItems="flex-start">
+          <HStack>
+            <Text color="gray.500">{degree}</Text>
+            <Text fontWeight="thin">|</Text>
+            <Text as="i" fontWeight="thin">{`${start} - ${end}`}</Text>
+          </HStack>
         </Flex>
       </Flex>
-    );
-  }
-}
+    </Flex>
+  );
+};
 
 export default EducationPreview;
--- src/components/ResumePreview.jsx
+++ src/components/ResumePreview.jsx
@@ -1,64 +1,59 @@
-import React, { Component } from "react";
+import React from "react";
 import { Flex, Heading } from "@chakra-ui/react";
 import ProfileSectionPreview from "./ProfileSectionPreview.jsx";
 import ExperiencePreview from "./ExperiencePreview.jsx";
 import EducationPreview from "./EducationPreview.jsx";
 import SkillsPreview from "./SkillsPreview.jsx";
-class ResumePreview extends Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    const { profile, workExperience, education, skills } = this.props;
-    return (
-      <Flex
-        direction="column"
-        border="2px solid"
-        borderColor="gray.700"
-        p={8}
-        borderRadius={5}
-        w="595px"
-        bgColor="white"
-        color="black"
-        alignItems="center"
-        height="fit-content"
-      >
-        <ProfileSectionPreview profile={profile} />
 
-        <Flex w="100%" pt={3} borderBottom="2px solid" borderColor="blue.700">
-          <Heading size="md" fontWeight="medium">
-            EXPERIENCE
-          </Heading>
-        </Flex>
+const ResumePreview = ({ profile, workExperience, education, skills }) => {
+  return (
+    <Flex
+      direction="column"
+      border="2px solid"
+      borderColor="gray.700"
+      p={8}
+      borderRadius={5}
+      w="595px"
+      bgColor="white"
+      color="black"
+      alignItems="center"
+      height="fit-content"
+    >
+      <ProfileSectionPreview profile={profile} />
 
-        {workExperience.map((exp) => (
-          <ExperiencePreview experience={exp} key={exp.id} id={exp.id} />
-        ))}
-
-        <Flex w="100%" pt={3} borderBottom="2px solid" borderColor="blue.700">
-          <Heading size="md" fontWeight="medium">
-            EDUCATION
-          </Heading>
-        </Flex>
-
-        {education.map((school) => (
-          <EducationPreview education={school} key={school.id} id={school.id} />
-        ))}
-
-        <Flex w="100%" pt={3} borderBottom="2px solid" borderColor="blue.700">
-          <Heading size="md" fontWeight="medium">
-            SKILLS
-          </Heading>
-        </Flex>
-
-        <Flex gap={4} mt={3} wrap="wrap">
-          {skills.map((skill) => (
-            <SkillsPreview skill={skill.skill} key={skill.id} id={skill.id} />
-          ))}
-        </Flex>
+      <Flex w="100%" pt={3} borderBottom="2px solid" borderColor="blue.700">
+        <Heading size="md" fontWeight="medium">
+          EXPERIENCE
+        </Heading>
       </Flex>
-    );
-  }
-}
+
+      {workExperience.map((exp) => (
+        <ExperiencePreview experience={exp} key={exp.id} id={exp.id} />
+      ))}
+
+      <Flex w="100%" pt={3} borderBottom="2px solid" borderColor="blue.700">
+        <Heading size="md" fontWeight="medium">
+          EDUCATION
+        </Heading>
+      </Flex>
+
+      {education.map((school) => (
+        <EducationPreview education={school} key={school.id} id={school.id} />
+      ))}
+
+      <Flex w="100%" pt={3} borderBottom="2px solid" borderColor="blue.700">
+        <Heading size="md" fontWeight="medium">
+          SKILLS
+        </Heading>
+      </Flex>
+
+      <Flex gap={4} mt={3} wrap="wrap">
+        {skills.map((skill) => (
+          <SkillsPreview skill={skill.skill} key={skill.id} id={skill.id} />
+        ))}
+      </Flex>
+    </Flex>
+  );
+};
 
 export default ResumePreview;
