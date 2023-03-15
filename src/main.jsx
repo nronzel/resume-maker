326be8d1222f4e92e1eab29e2dd440eb71490d23
nronzel <86695181+nronzel@users.noreply.github.com>
2023-03-15 13:31:38 -0400

    fixed issue with initial theme not applying

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import theme from "./theme.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <App />
     </ChakraProvider>
